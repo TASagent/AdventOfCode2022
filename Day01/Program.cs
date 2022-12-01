bc88ffa18ceded76b5df4283458a75df38cc7c47
TASagent <TASagent.Streams@gmail.com>
2022-11-30 21:33:26 -0800

    Adding Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,23 +1,22 @@
 ﻿const string inputFile = @"../../../../input01.txt";
 
-Console.WriteLine("Day 01 - ???");
+Console.WriteLine("Day 01 - Calorie Counting");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
 //string[] lines = File.ReadAllLines(inputFile);
-//string line = File.ReadAllText(inputFile);
+IEnumerable<int> elfCalorieTotal = File.ReadAllText(inputFile)
+    .Split("\r\n\r\n")
+    .Select(x => x.Split("\r\n").Select(int.Parse).Sum())
+    .ToList();
 
-int value = 0;
-
-Console.WriteLine($"The answer is: {value}");
+Console.WriteLine($"The answer is: {elfCalorieTotal.Max()}");
 
 Console.WriteLine();
 Console.WriteLine("Star 2");
 Console.WriteLine();
 
-int value2 = 0;
-
-Console.WriteLine($"The answer is: {value2}");
+Console.WriteLine($"The answer is: {elfCalorieTotal.OrderByDescending(x=>x).Take(3).Sum()}");
 
 Console.WriteLine();
 Console.ReadKey();
