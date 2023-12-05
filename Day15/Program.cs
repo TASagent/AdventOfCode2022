8eeb3c5ceddc947e5b509ba9880782e746962dc0
TASagent <TASagent.Streams@gmail.com>
2023-12-05 07:18:39 -0600

    Partial implementation of Day15

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,13 +1,61 @@
-﻿const string inputFile = @"../../../../input15.txt";
+﻿using AoCTools;
+using System.Text.RegularExpressions;
 
-Console.WriteLine("Day 15 - ???");
+const string inputFile = @"../../../../input15.txt";
+
+Console.WriteLine("Day 15 - Beacon Exclusion Zone");
 Console.WriteLine("Star 1");
 Console.WriteLine();
 
-//string[] lines = File.ReadAllLines(inputFile);
-//string line = File.ReadAllText(inputFile);
+const int targetY = 10;
 
-int value = 0;
+string[] lines = File.ReadAllLines(inputFile);
+
+HashSet<LongPoint2D> sensors = new();
+Dictionary<LongPoint2D, LongPoint2D> closestBeacons = new();
+
+Regex captureCoordinates = new Regex(@"x=([-0-9]+), y=([-0-9]+)");
+
+foreach (string line in lines)
+{
+    string[] splitLine = line.Split(':');
+
+    LongPoint2D sensorLocation = new LongPoint2D(
+        x: long.Parse(captureCoordinates.Match(splitLine[0]).Groups[1].Value),
+        y: long.Parse(captureCoordinates.Match(splitLine[0]).Groups[2].Value));
+
+    LongPoint2D beaconLocation = new LongPoint2D(
+        x: long.Parse(captureCoordinates.Match(splitLine[1]).Groups[1].Value),
+        y: long.Parse(captureCoordinates.Match(splitLine[1]).Groups[2].Value));
+
+    sensors.Add(sensorLocation);
+
+    closestBeacons.Add(sensorLocation, beaconLocation);
+}
+
+HashSet<LongPoint2D> excludedBeaconPositions = new();
+
+foreach (LongPoint2D sensor in sensors)
+{
+    long totalDistance = (sensor - closestBeacons[sensor]).TaxiCabLength;
+
+    long deltaY = Math.Abs(targetY - sensor.y);
+
+    if (deltaY > totalDistance)
+        continue;
+
+    excludedBeaconPositions.Add((sensor.x, targetY));
+
+    for (int i = 1; i <= (totalDistance - deltaY); i++)
+    {
+        excludedBeaconPositions.Add((sensor.x + i, targetY));
+        excludedBeaconPositions.Add((sensor.x - i, targetY));
+    }
+}
+
+
+
+int value = excludedBeaconPositions.Except(closestBeacons.Values).Count();
 
 Console.WriteLine($"The answer is: {value}");
 
